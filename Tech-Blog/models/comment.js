ba38c0765d4f9abc868483000597594a6fed704e
jbarker <jackson.barker@du.edu>
2021-12-03 22:31:04 -0700

    comment, and handlebars update

--- Tech-Blog/models/comment.js
+++ Tech-Blog/models/comment.js
@@ -11,7 +11,7 @@ Comment.init(
         primaryKey: true,
         autoIncrement: true,
       },
-      commentText: {
+      commentContent: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
--- Tech-Blog/public/js/profile.js
+++ Tech-Blog/public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-name').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const title = document.querySelector('#blog-name').value.trim();
+  const content = document.querySelector('#blog-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && content) {
     const response = await fetch(`/api/blog`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
